bbe92f08d6a2a8e86149468091eae3aa15535c28
Andriy Astakhov <andrey.astakhov@gmail.com>
2026-03-03 18:06:32 +0100

    Extract notification to twig

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\AI\AiBundle\AiBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
