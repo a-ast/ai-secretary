059fc8dd6557e7e5b6f1716efd958d5e1d971331
Andriy Astakhov <andrey.astakhov@gmail.com>
2026-03-04 17:31:07 +0100

    Restore Twig template, switch to HTML parse mode to fix double-escaping

--- src/Adapters/Telegram/TelegramNotificationSender.php
+++ src/Adapters/Telegram/TelegramNotificationSender.php
@@ -9,24 +9,20 @@ use App\Domain\Port\NotificationSenderInterface;
 use Symfony\Component\Notifier\Bridge\Telegram\TelegramOptions;
 use Symfony\Component\Notifier\ChatterInterface;
 use Symfony\Component\Notifier\Message\ChatMessage;
+use Twig\Environment;
 
 final readonly class TelegramNotificationSender implements NotificationSenderInterface
 {
-    public function __construct(private ChatterInterface $chatter) {}
+    public function __construct(
+        private ChatterInterface $chatter,
+        private Environment $twig,
+    ) {}
 
     public function send(ActionItem $item): void
     {
-        $e = fn (string $s) => htmlspecialchars($s, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+        $text = $this->twig->render('telegram/action-item.html.twig', ['item' => $item]);
 
-        $text = sprintf(
-            "📩 <b>New Email</b>\n\n<b>Subject:</b> <a href=\"%s\">%s</a>\n\n<b>Summary:</b> %s\n\n<b>Reason:</b> %s",
-            $e($item->link),
-            $e($item->title),
-            $e($item->summary),
-            $e($item->reason),
-        );
-
-        $message = new ChatMessage($text);
+        $message = new ChatMessage(trim($text));
         $message->options(new TelegramOptions()->parseMode('HTML'));
 
         $this->chatter->send($message);
